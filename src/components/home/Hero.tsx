b41817022627e54e897ca70ebaca4ff1ef5afc62
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-22 18:34:27 +0700

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -47,7 +47,7 @@ export default function Hero() {
 
           </Link>
 
-          <nav className="hidden lg:flex items-center gap-10 font-medium text-slate-700">
+          <nav className="flex items-center gap-10 font-medium text-slate-700">
 
             <Link href="/"
               className="text-slate-700 transition hover:text-cyan-600"
@@ -82,7 +82,7 @@ export default function Hero() {
 
           </nav>
 
-          <div className="hidden lg:flex items-center gap-4">
+          <div className="flex items-center gap-4">
 
             <Link
   href="/login"
