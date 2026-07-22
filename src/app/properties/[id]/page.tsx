e6c308cad8a191f03440f03a4e437905e3b82e05
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-22 23:22:56 +0700

    Update page.tsx

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -4,6 +4,7 @@ import PropertySummary from "@/components/property/PropertySummary";
 import PropertySpecification from "@/components/property/PropertySpecification";
 import PropertyDescription from "@/components/property/PropertyDescription";
 import PropertyAgentCard from "@/components/property/PropertyAgentCard";
+import PropertyMap from "@/components/property/PropertyMap";
 
 const properties = [
   {
@@ -84,21 +85,7 @@ export default async function PropertyDetailPage({
 
 <div className="mt-8 grid gap-8 lg:grid-cols-[2fr_380px]">
 
-  <div>
-
-    <PropertySpecification
-      bedroom={property.bedroom}
-      bathroom={property.bathroom}
-      land={property.land}
-      building={property.building}
-    />
-
-    <PropertyDescription
-      description={property.description}
-    />
-
-  </div>
-
+  import PropertyMap from "@/components/property/PropertyMap";
   <PropertyAgentCard />
 
 </div>
